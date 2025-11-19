15fd90a4d6706debbffbd16b72f94a5128f3f5bb
Adrian Mihu <adrian.mihu20@icloud.com>
2025-11-20 01:16:20 +0200

    Project clean-up and new Fabric initialization

---
+++ src/main/java/ro/maleficent/tunnelertnt/TunnelerTNT.java
@@ -0,0 +1,16 @@
+package ro.maleficent.tunnelertnt;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TunnelerTNT implements ModInitializer {
+	public static final String MOD_ID = "tunnelertnt";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/ro/maleficent/tunnelertnt/TunnelerTNTClient.java
@@ -0,0 +1,11 @@
+package ro.maleficent.tunnelertnt;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TunnelerTNTClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
